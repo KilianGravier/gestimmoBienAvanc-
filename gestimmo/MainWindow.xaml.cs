0f5257ccfa3dd0dc0998735e36494094c8f65298
Kilian Gravier <k.gravier@sio.local>
2023-11-15 11:42:08 +0100

    mise à jour 2

--- gestimmo/MainWindow.xaml.cs
+++ gestimmo/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,33 +15,59 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Serilog.Formatting.Compact;
+
 
 namespace gestimmo_WPF
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MainWindow.xaml heho c'est moi le bon
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-            // Charge la page d'accueil au démarrage
-            MainFrame.Navigate(new Uri("MainWindow.xaml", UriKind.Relative));
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.ControlledBy(new LoggingLevelSwitch(LogEventLevel.Debug)) // Niveau minimum à Debug
+                .WriteTo.Logger(lc => lc.Filter.ByIncludingOnly(e => e.Level == LogEventLevel.Debug).WriteTo.Console()) // Seuls les niveaux Debug s'affichent dans la console
+                .WriteTo.File("myapp.txt", restrictedToMinimumLevel: LogEventLevel.Verbose) // Tous les niveaux sont enregistrés dans le fichier
+                .CreateLogger();
+
+            Log.Information("Hello, world!");
         }
+
+
+
+
+
+
+
+
 
         private void BiensButton_Click(object sender, RoutedEventArgs e)
         {
             // Charge la page Biens lors du clic sur le bouton Biens
             MainFrame.Navigate(new Uri("BiensView.xaml", UriKind.Relative));
         }
+
+        // mit en cyber tp logs
+        
+
+
     }
 
-
-
-
-
-
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- gestimmo/obj/Debug/net6.0-windows/Views/SubViews/HomeView.g.i.cs
+++ gestimmo/obj/Debug/net6.0-windows/Views/SubViews/HomeView.g.i.cs
@@ -53,7 +53,7 @@ namespace gestimmo_WPF.Views.SubViews {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/gestimmo WPF;V1.0.0.0;component/views/subviews/homeview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/gestimmo WPF;component/views/subviews/homeview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\Views\SubViews\HomeView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
